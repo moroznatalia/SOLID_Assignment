0c52129ec36f9b99e45f3540de92112d0e1b0fb5
Natalia Moroz <nataliamoroz.mm@gmail.com>
2023-03-11 20:15:31 +0200

    SOLID solution with weak D without spaces in main

--- src/ro/ase/cts/main/Main.java
+++ src/ro/ase/cts/main/Main.java
@@ -14,14 +14,11 @@ public class Main {
     public static void main(String[] args) {
         try {
             Connection connection = new ConnectionCreator().createConnection();
-
-
             new TableCreator().createTable(connection);
             new DataInserter().insertData(connection);
             new DataInserterWithParams().insertData(connection);
             ResultSet resultSet = new DataReader().readData(connection);
             new SystemOutDataWriter().displayResult(resultSet);
-
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
